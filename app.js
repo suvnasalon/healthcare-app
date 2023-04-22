e61b64cfd41e29133af3cf7dcdc5f29c4f881899
sheng zhang <shengzhang07@gmail.com>
2023-04-21 22:36:17 -0700

    Simplify node implementation using express js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const http = require('http');
-const fs = require('fs');
-const port=process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
+require('dotenv').config();
 
-    fs.readFile("appform.html", function(err, text){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(text);        
-    });
-});
-server.listen(port,() => {
-    console.log(`Server running at port `+port);
-});
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port=process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/appform.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
